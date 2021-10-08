e8b3507587ab043adfd95f26cec63356dc43e350
Class account - UAL <ualclsa0008@dmcvlogin4.asc.edu>
2021-10-08 17:17:44 -0500

    add result

--- Homework#3/script_1000_5000.sh
+++ Homework#3/script_1000_5000.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ./life 1000 5000 1 /scratch/$USER
-./hw3 1000 100 1 /scratch/$USER
-./hw3 1000 100 2 /scratch/$USER
-./hw3 1000 100 4 /scratch/$USER
-./hw3 1000 100 8 /scratch/$USER
-./hw3 1000 100 10 /scratch/$USER
-./hw3 1000 100 16 /scratch/$USER
-./hw3 1000 100 20 /scratch/$USER
+./hw3 1000 5000 1 /scratch/$USER
+./hw3 1000 5000 2 /scratch/$USER
+./hw3 1000 5000 4 /scratch/$USER
+./hw3 1000 5000 8 /scratch/$USER
+./hw3 1000 5000 10 /scratch/$USER
+./hw3 1000 5000 16 /scratch/$USER
+./hw3 1000 5000 20 /scratch/$USER
