06c9975afd8e1bf85c6ff579101ba4b4568cf1d4
yhuang87 <yhuang87@crimson.ua.edu>
2021-11-09 18:44:37 -0600

    Update hw5.c

--- Homework#5/hw5.c
+++ Homework#5/hw5.c
@@ -13,7 +13,6 @@
 #include <assert.h>
 
 #define MAXN 1048576
-#define NTIMES 3
 
 int allgather(void *sendbuf, int sendcount, MPI_Datatype sendtype, void *recvbuf, int recvcount, MPI_Datatype recvtype, MPI_Comm comm);
 
@@ -34,16 +33,44 @@ int main (int argc, char **argv) {
     }
     recvbuf = (int *) malloc (sizeof(int) * MAXN * size);
 
-    
+    for (msgsize = 32; msgsize <= MAXN; msgsize = msgsize << 1) {
 
+        MPI_Barrier(MPI_COMM_WORLD);
+        t1 = MPI_Wtime();
 
-    
+        allgather(sendbuf,msgsize,MPI_INT,recvbuf,msgsize,MPI_INT,MPI_COMM_WORLD);
 
-    
+        t2 = MPI_Wtime() - t1;
+        MPI_Reduce(&t2, &t1, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
 
+        //check results
+        for(i = 0; i<msgsize*size; i++) {
+            offset = i%msgsize;
+            assert(recvbuf[i] == offset);
+        }
 
+        if(rank == 0) {
+            printf("Message size = %ld bytes, Maximum time = %g\n",msgsize * sizeof(int), t1/100);
+        }
+    }
 
     MPI_Finalize ();
 
     return 0;
+}
+
+int allgather(void *sendbuf, int sendcount, MPI_Datatype sendtype, void *recvbuf, int recvcount, MPI_Datatype recvtype, MPI_Comm comm) {
+    int rank, size, i, offset;
+    MPI_Status *status;
+    MPI_Request *request;
+    MPI_Aint lb, sizeofsendtype, sizeofrecvtype;
+
+    MPI_Comm_rank(comm, &rank);
+    MPI_Comm_size(comm, &size);
+
+    status = malloc (sizeof(MPI_Status) * (size + 1));
+    request = malloc (sizeof(MPI_Request) * (size + 1));
+
+    
+
 }
